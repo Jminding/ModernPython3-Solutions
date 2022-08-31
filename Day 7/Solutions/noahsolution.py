e6bceb13401fb0cde9af060e5565751432cc29d6
Jminding <jtding43@gmail.com>
2022-08-31 15:31:26 -0400

    Noah's Solution

--- Day 7/Solutions/noahsolution.py
+++ Day 7/Solutions/noahsolution.py
@@ -1,9 +1,13 @@
-class cookie:
+class Cookie:
     def __init__(self, type, brand,color ):
         self.type = type
         self.brand = brand
         self.color = color
-        def __str__(self):
-            return f"{self.brand} {self.type} cookie is {self.color}, very {self.color}"
-            oreo =("oreo","dark chocolate","black")
-print(oreo)
+    def __str__(self):
+        return f"{self.brand} {self.type} cookie is {self.color}, very {self.color}"
+    def oreoreo(self):
+        while True:
+            if self.brand == "oreo":
+                print("oreo")
+oreo = Cookie("oreo","dark chocolate","black")
+print(oreo) 
